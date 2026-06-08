39adff1be651a6f63792c1b8abfb34a815ac0e42
P. E. A. Lutz <pea@misfist.com>
2026-06-08 01:37:16 -0400

    Tooling

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,62 @@
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
 const path = require( 'path' );
 
-module.exports = {
-	...defaultConfig,
-	entry: {
-		...defaultConfig.entry,
-		index: path.resolve( __dirname, 'src/scss/style.scss' ),
-		editor: path.resolve( __dirname, 'src/scss/editor.scss' ),
+const baseConfig = Array.isArray( defaultConfig ) ? defaultConfig[ 0 ] : defaultConfig;
+
+const copyPlugin = new CopyWebpackPlugin( {
+	patterns: [
+		{
+			from: 'src/fonts',
+			to: 'fonts',
+			noErrorOnMissing: true,
+		},
+		{
+			from: 'src/images',
+			to: 'images',
+			noErrorOnMissing: true,
+		},
+		{
+			from: 'node_modules/@wordpress/icons/src/library/*.svg',
+			to: 'icons/[name][ext]',
+			noErrorOnMissing: true,
+		},
+	],
+} );
+
+module.exports = [
+	{
+		...baseConfig,
+		entry: {
+			...baseConfig.entry,
+			index: [
+				path.resolve( __dirname, 'src/index.scss' ),
+				path.resolve( __dirname, 'src/index.js' ),
+			],
+			editor: path.resolve( __dirname, 'src/editor.scss' ),
+		},
+		plugins: [
+			...( baseConfig.plugins || [] ),
+			copyPlugin,
+		],
 	},
-};
+	{
+		mode: baseConfig.mode,
+		entry: {
+			interactivity: path.resolve( __dirname, 'src/js/interactivity/index.js' ),
+		},
+		output: {
+			filename: '[name].js',
+			path: path.resolve( __dirname, 'build' ),
+			library: { type: 'module' },
+			clean: false,
+		},
+		externalsType: 'module',
+		externals: {
+			'@wordpress/interactivity': '@wordpress/interactivity',
+		},
+		experiments: {
+			outputModule: true,
+		},
+	},
+];
