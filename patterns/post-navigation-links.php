b5d5b0091ddb58f3475e50f231056e33aa0f195e
P. E. A. Lutz <pea@misfist.com>
2026-06-16 02:28:29 -0400

    Render patterns in templates

---
+++ patterns/post-navigation-links.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Post Navigation Links
+ * Slug: pealutz/post-navigation-links
+ * Description: Single post navigation link
+ * Categories: pealutz
+ * Keywords: navigation
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"Post Navigation"},"className":"post-navigation","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group post-navigation">
+    <!-- wp:post-navigation-link {"type":"previous","showTitle":true,"linkLabel":true,"arrow":"chevron"} /-->
+
+    <!-- wp:post-navigation-link {"showTitle":true,"linkLabel":true,"arrow":"chevron"} /-->
+</div>
+<!-- /wp:group -->
